f5dcdaef36903f45e644dd393f68b1094e5fd3cf
HansBug <killog@126.com>
2018-05-14 15:24:24 +0800

    add Direction check

--- src/enums/Direction.java
+++ src/enums/Direction.java
@@ -6,7 +6,7 @@ import interfaces.application.ApplicationClassInterface;
  * 移动方向类
  */
 public enum Direction implements ApplicationClassInterface {
-    UP(-1, 0), DOWN(1, 0), LEFT(0, -1), RIGHT(0, 1);
+    UP(-1, 0, 0), DOWN(1, 0, 2), LEFT(0, -1, 1), RIGHT(0, 1, 3);
     
     /**
      * X增量
@@ -19,22 +19,31 @@ public enum Direction implements ApplicationClassInterface {
     private final int delta_y;
     
     /**
+     * 编号
+     */
+    private final int id;
+    
+    /**
      * 构造函数
      *
      * @param delta_x X增量
      * @param delta_y Y增量
+     * @param id      id编号
      */
-    private Direction(int delta_x, int delta_y) {
+    private Direction(int delta_x, int delta_y, int id) {
         /**
          * @modifies:
          *          \this.delta_x;
          *          \this.delta_y;
+         *          \this.id;
          * @effects:
          *          \this.delta_x == delta_x;
          *          \this.delta_y == delta_y;
+         *          \this.id == id;
          */
         this.delta_x = delta_x;
         this.delta_y = delta_y;
+        this.id = id;
     }
     
     /**
@@ -63,5 +72,143 @@ public enum Direction implements ApplicationClassInterface {
         return this.delta_y;
     }
     
+    /**
+     * 根据id查询方向
+     *
+     * @param id id
+     * @return 查询方向
+     */
+    private static Direction getDirectionById(int id) {
+        /**
+         * @effects:
+         *          (\ exist Direction direction : direction.id = = id) ==> \result == direction;
+         *          (\all Direction direction : direction.id != id) ==> \result == null;
+         */
+        for (Direction value : values()) {
+            if (value.id == id) return value;
+        }
+        return null;
+    }
     
+    /**
+     * 根据delta值获取方向
+     *
+     * @param delta_x delta x
+     * @param delta_y delta y
+     * @return 方向
+     */
+    public static Direction getDirectionByDelta(int delta_x, int delta_y) {
+        /**
+         * @effects:
+         *          (\ exists Direction direction : direction.delta_x = = delta_x & & direction.delta_y = = delta_y) ==> \result == direction;
+         *          (\all Direction direction : direction.delta_x != delta_x || direction.delta_y != delta_y) ==> \result == null;
+         */
+        for (Direction value : values()) {
+            if ((value.delta_x == delta_x) && (value.delta_y == delta_y)) return value;
+        }
+        return null;
+    }
+    
+    /**
+     * 方向常量
+     */
+    private static final int DIRECTION_COUNT = 4;
+    private static final int LEFT_OFFSET = 1;
+    private static final int BACK_OFFSET = 2;
+    private static final int RIGHT_OFFSET = 3;
+    
+    /**
+     * 逆时针旋转
+     *
+     * @param offset 旋转次数
+     * @return 旋转结果
+     */
+    private Direction turnLeft(int offset) {
+        /**
+         * @effects:
+         *          \result.id == (\this.id + offset) % DIRECTION_COUNT;
+         */
+        return getDirectionById((this.id + offset) % DIRECTION_COUNT);
+    }
+    
+    /**
+     * 获取反方向
+     *
+     * @return 反方向
+     */
+    public Direction getBack() {
+        /**
+         * @effects:
+         *          \result.id == (\this.id + BACK_OFFSET) % DIRECTION_COUNT;
+         */
+        return turnLeft(BACK_OFFSET);
+    }
+    
+    /**
+     * 判断另一个是否为反方向
+     *
+     * @param direction 另一个方向
+     * @return 是否为反方向
+     */
+    public boolean isBack(Direction direction) {
+        /**
+         * @effects:
+         *          \result == (direction.id == (\this.id + BACK_OFFSET) % DIRECTION_COUNT);
+         */
+        return direction == getBack();
+    }
+    
+    /**
+     * 获取左转方向
+     *
+     * @return 左转方向
+     */
+    public Direction getLeft() {
+        /**
+         * @effects:
+         *          \result.id == (\this.id + LEFT_OFFSET) % DIRECTION_COUNT;
+         */
+        return turnLeft(LEFT_OFFSET);
+    }
+    
+    /**
+     * 判断另一个方向是否为左转方向
+     *
+     * @param direction 另一个方向
+     * @return 是否为左转方向
+     */
+    public boolean isLeft(Direction direction) {
+        /**
+         * @effects:
+         *          \result == (direction.id  == (\this.id + LEFT_OFFSET) % DIRECTION_COUNT);
+         */
+        return direction == getLeft();
+    }
+    
+    /**
+     * 获取右转方向
+     *
+     * @return 右转方向
+     */
+    public Direction getRight() {
+        /**
+         * @effects:
+         *          \result.id == (\this.id + RIGHT_OFFSET) % DIRECTION_COUNT;
+         */
+        return turnLeft(RIGHT_OFFSET);
+    }
+    
+    /**
+     * 判断另一个方向是否为右转方向
+     *
+     * @param direction 另一个方向
+     * @return 是否为右转方向
+     */
+    public boolean isRight(Direction direction) {
+        /**
+         * @effects:
+         *          \result == (direction.id == (\this.id + RIGHT_OFFSET) % DIRECTION_COUNT);
+         */
+        return direction == getRight();
+    }
 }
